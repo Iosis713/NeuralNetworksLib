233a584dd7445d8e14419bdb9e9992998149744e
Bartosz Lozinski <lozinski.bart@gmail.com>
2025-10-26 18:35:35 +0100

    Code cleaned

--- main.cpp
+++ main.cpp
@@ -8,19 +8,26 @@ int main()
     
     InputLayer inputLayer{{InputNeuron{0.75}, InputNeuron{0.5}}};
 
-    NeuralNet neuralNet{inputLayer, {3, 2}, 2};
-    
-    std::cout << "\nBefore Forward algorithm:\n";
-    neuralNet.Print();
-    neuralNet.PrintWeights();
+    try
+    {
+        NeuralNet neuralNet{inputLayer, {3, 4}, 2};
+        
+        std::cout << "\nBefore Forward algorithm:\n";
+        neuralNet.Print();
+        neuralNet.PrintWeights();
 
-    std::cout << "\nAfter first run of forward algorithm:\n";
-    neuralNet.Forward();
-    neuralNet.Print();
+        std::cout << "\nAfter first run of forward algorithm:\n";
+        neuralNet.Forward();
+        neuralNet.Print();
 
-    std::cout << "\nAfter second run of forward algorithm:\n";
-    neuralNet.Forward();
-    neuralNet.Print();
+        std::cout << "\nAfter second run of forward algorithm:\n";
+        neuralNet.Forward();
+        neuralNet.Print();
+    }
+    catch(std::runtime_error& err)
+    {
+        std::cout << err.what() << '\n';
+    }
 
     return 0;
 }
